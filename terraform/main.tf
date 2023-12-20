b316b5bf9f3800c0758776bcf9d3011f003a37b6
warapong-pj <warapong.pj@gmail.com>
2023-12-20 20:25:39 +0700

    deploy sqs and policy sqs

--- terraform/main.tf
+++ terraform/main.tf
@@ -100,3 +100,37 @@ module "iam_s3_policy" {
 }
 EOF
 }
+
+
+module "sqs" {
+  source  = "terraform-aws-modules/sqs/aws"
+  version = "4.1.0"
+
+  name = "lms-import-data"
+}
+
+module "iam_sqs_policy" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "5.33.0"
+
+  name        = "eks-access-to-sqs"
+  path        = "/"
+
+  policy = <<EOF
+{
+	"Version": "2012-10-17",
+	"Statement": [
+		{
+			"Sid": "VisualEditor0",
+			"Effect": "Allow",
+			"Action": [
+				"sqs:ReceiveMessage",
+				"sqs:DeleteQueue",
+				"sqs:SendMessage"
+			],
+			"Resource": "arn:aws:sqs:ap-southeast-1:XXXXXXXXXXXX:lms-import-data"
+		}
+	]
+}
+EOF
+}
